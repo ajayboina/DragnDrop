c29e0937c233d84494f70f189513c5196f828a42
ajay nagaboina <ajay.nagaboina@lssinc.com>
2017-01-02 20:47:03 +0530

    Add project files.

---
+++ DragnDrop/Scripts/DnDusingDirective.js
@@ -0,0 +1,48 @@
+﻿/// <reference path="Angular/angular.js" />
+var myApp = angular.module("DragnDrop", ["kendo.directives"]);
+
+myApp.controller("myController", function ($scope) {
+
+    $scope.draggableHint = function () {
+        return $("#draggable").clone();
+    }
+
+  
+
+    $scope.headerInfo = function () {
+        var myHeaders = [];
+        myHeaders.push("First Name");
+        myHeaders.push("Last Name");
+        myHeaders.push("Salary");
+        return myHeaders;
+    }
+
+    $scope.rowInfo = function () {
+        var myRowIno = [];
+        myRowIno.push("Ajay");
+        myRowIno.push("Nag");
+        myRowIno.push("23");
+    }
+
+    $scope.onDragStart = function () {
+        $scope.$apply(function () {
+            $scope.draggableClass = "hollow";
+            $scope.dropTargetText = "Drop here.";
+        });
+    }
+
+    $scope.onDrop = function (e) {
+        $scope.$apply(function () {
+            $scope.IsVisible = $scope.IsVisible ? false : true;
+            $scope.draggableClass = "";
+        });
+    }
+})
+
+.directive("myEmployee", function () {
+    return {
+        restrict: 'E',
+        isVisible:'false',
+        templateUrl: 'EmpDnD.html',
+    };
+});
---
+++ DragnDrop/Scripts/DragnDrop.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="angular.js" />
+var myApp = angular.module("DragnDrop", ["kendo.directives"]);
+myApp.controller("myController", function ($scope) {
+
+    $scope.draggableHint = function () {
+        return $("#draggable").clone();
+    }
+    $scope.onDragStart = function () {
+        $scope.$apply(function () {
+            $scope.draggableClass = "hollow";
+            $scope.dropTargetText = "Drop here.";
+        });
+    }
+
+    $scope.onDrop = function (e) {
+        $scope.$apply(function () {
+            $scope.draggableClass = "";
+        });
+    }
+});
+
+
